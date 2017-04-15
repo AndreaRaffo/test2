b4f2fc2a0bfa802347726cfa1de5728b7285117f
AndreaRaffo <andrea.raffo96@hotmail.it>
2017-04-15 22:03:31 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,12 +1,8 @@
 <?php
 if(isset($_POST["name"]) && isset($_POST["password"])){
 	
-	$file = fopen('data.txt', 'a');
-	fwrite($f,'Hello;123');
-    fclose($f);
-	include('index.php');
 	
-/*    $file = fopen('data.txt', 'r');
+   $file = fopen('data.txt', 'r');
     $good=false;
     while(!feof($file)){
 			$line = fgets($file);
@@ -25,7 +21,7 @@ if(isset($_POST["name"]) && isset($_POST["password"])){
 		echo "<script> alert('$message'); </script>";
 		include('index.php');
     }
-fclose($file);*/
+fclose($file);
 }else{
     include 'index.php';
 }
