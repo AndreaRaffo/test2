3a3d0ae66049d4e2ce18bd92bb308d0775c0a00a
AndreaRaffo <andrea.raffo96@hotmail.it>
2017-04-15 21:19:04 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST["name"]) && isset($_POST["password"])){
     $f=fopen("data.txt","a");
-    fputs($f,$_POST["username"].";".$_POST["password"]."\r\n");
+    fputs($f,$_POST["name"].";".$_POST["password"]."\r\n");
     fclose($f);
 }else{
     include 'index.php';
