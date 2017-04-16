d583adb132d5d0719c73994aa04b4078864f214b
AndreaRaffo <andrea.raffo96@hotmail.it>
2017-04-16 19:59:57 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 
 if(1>0){
   $username = $_POST['username'];
-	echo '$username';
+	echo $username;
   $password = $_POST['password'];
   $text = $username . ";" . $password . "\n";
   $fp = fopen('data.txt', 'a+');
