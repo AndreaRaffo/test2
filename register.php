95190a99505024e19443dc59e17b381691614035
AndreaRaffo <andrea.raffo96@hotmail.it>
2017-04-16 20:04:34 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -5,12 +5,13 @@ if(1>0){
 	echo $username;
   $password = $_POST['password'];
   $text = $username . ";" . $password . "\n";
-  $fp = fopen('data.txt', 'a+');
+  $fp = fopen('data.txt', 'a');
 
     if(fwrite($fp, $text))  {
         echo 'saved';
     }
-fclose ($fp);    
+fclose ($fp);  
+	include 'index.php';
 }
 else{
 	include 'index.php';
