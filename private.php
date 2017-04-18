337d09c1f64e72ed5364c3af800bb2a2cc068851
AndreaRaffo <andrea.raffo96@hotmail.it>
2017-04-18 11:06:27 +0200

    Add files via upload

--- private.php
+++ private.php
@@ -1,20 +1,59 @@
 
+
 <?php
 session_start();
+if(!isset($_SESSION['name'])){
+	header("Location: index.php");
+}
 ?>
-
 
 <!doctype html>
-<html>
+<html lang="en">
 <head>
 <meta charset="utf-8">
-<title>MyPage</title>
+	<title>Civic Hacking</title>
+	<link href="style.css" rel = "stylesheet" type = "text/css"/>
 </head>
 
-<?php
-print_r($_SESSION);
-?>
-
 <body>
+<div id="header">
+	<div class="container">
+		<div id="logoArea">
+		</div>
+		<div id="navArea">
+			<ul id="nav">
+				<li><a href="index.php"><img src="Images/home.png" alt="Home"></a></li>
+				<li><a href="#"><img src="Images/descrizione.png" alt="Descrizione"></a></li>
+				<li><a href="#"><img src="Images/servizi.png" alt="Servizi"></a></li>
+				<li><a href="#"><img src="Images/contatti.png" alt="contatti"></a></li>	
+			</ul>
+			
+		  <a href="logout.php" id="logout"><img src="Images/logout.png" alt="Logout"></a>
+																	
+			</div>
+		</div>
+	</div>
+	
+
+
+	<div id="mainArea">
+		<div class="container page">
+			<h3>Sito in costruzione</h3>
+			<?php
+			$file = fopen('data.txt', 'r');
+			while(!feof($file)){
+			$line = fgets($file);
+			echo "$line<br>";
+			}
+			?>
+		</div>
+	</div>
+	
+	<div id="footer">
+		<div class="container">
+			Sito di Marco Borinato e Andrea Raffo
+		</div>
+	</div>
+
 </body>
 </html>
